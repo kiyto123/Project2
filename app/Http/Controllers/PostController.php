7aaa175a3b9a8b2aa5e45d8d1b5916eafa7ee060
Montri Kosasod <kiyto123@gmail.com>
2020-08-16 16:59:05 +0700

    finish

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,38 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('user_id',auth()->user()->id)->get();
+        $posts = Post::where('user_id', Auth::user()->id)->paginate(5);
         $data = [
             'posts' => $posts
         ];
         return view('post.index', $data);
     }
+
     public function show($id)
     {
         $category = Post::find($id);
         return $category;
     }
+
     public function create()
     {
-        //ข้างในนี้ไม่มีอะไร
+        $categories = Category::all();
+        $data = [
+            'categories' => $categories
+        ];
+        return view('post.create', $data);
     }
-    public function store()
+
+    public function store(Request $request)
     {
-        //ข้างในนี้ไม่มีอะไร
+        $detail = $request->input('detail');
+        $category_id = $request->input('category_id');
+
+        $post = new Post();
+        $post->detail = $detail;
+        $post->category_id = $category_id;
+        $post->user_id = auth()->user()->id;
+        $post->complete = 0;
+        $post->save();
+
+        return redirect('/post');
     }
+
     public function edit($id)
     {
-        //ข้างในนี้ไม่มีอะไร
     }
-    public function update($id)
+
+    public function delete($id)
     {
-        //ข้างในนี้ไม่มีอะไร
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/post');
     }
 }
